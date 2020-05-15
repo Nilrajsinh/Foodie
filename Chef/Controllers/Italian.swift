56931d9bec4abc4450ca1eb72d8401835af51282
Nilrajsinh <Nilrajsinh1@icloud.com>
2020-05-15 23:24:45 +0530

    Only Chinese Left

--- Chef/Controllers/Italian.swift
+++ Chef/Controllers/Italian.swift
@@ -17,13 +17,42 @@ class Italian: UICollectionViewController {
           }
     
     var Italian = [
-     ["Title":"Jalebi ","Image":#imageLiteral(resourceName: "Jalebi-Recipe-Rasoi-Menu"),"URL":"","Des":""],
+     ["Title":"Caprese Salad with Pesto Sauce","Image":#imageLiteral(resourceName: "caprese-salad_625x350_51506417724.jpg"),"URL":"","Des":"Nothing like a fresh tomato salad in summers! A great antipasto bite to start your meal with. This combination of juicy tomatoes and mozzarella cheese salad topped with freshly made pesto sauce is a distinct yet simple one. It offers a twist to the classic caprese salad"],
                   
-                   ["Title":"Medu wada ","Image":#imageLiteral(resourceName: "South_Indian_Medu_Vada_Sambar_with_Medu_Vada_Maker-8_400"),"URL":"","Des":""],
+                   ["Title":"Panzenella","Image":#imageLiteral(resourceName: "panzenella_600x300_71506417795.jpg"),"URL":"","Des":"Panzenella is a Tuscan bread salad, ideal for summer. It does not follow a particular recipe, but the two ingredients that do not change are tomatoes and bread. This salad is great with a chilled glass of Prosecco and lots of sunshine!"],
                    
-                    ["Title":"Raas malai ","Image":#imageLiteral(resourceName: "Kesar-Rasmalai"),"URL":"","Des":""],
+                    ["Title":"Bruschetta","Image":#imageLiteral(resourceName: "bruschetta_625x350_71506417841.jpg"),"URL":"","Des":"An antipasto dish, bruschetta has grilled bread topped with veggies, rubbed garlic and tomato mix. A country bread sliced and topped with different toppings - the evergreen tomato-basil and an inventive mushroom-garlic. The classic Italian starter!"],
                     
-                     ["Title":"Uthapam ","Image":#imageLiteral(resourceName: "big_onion_tomato_uttapam,_tomato_onion_uttapa-11571"),"URL":"","Des":""] ]
+                     ["Title":"Margherita Pizza","Image":#imageLiteral(resourceName: "South_Indian_Medu_Vada_Sambar_with_Medu_Vada_Maker-8_400"),"URL":"","Des":"Fancy a pipping hot pizza, fresh out of the oven? Create one at home! Margherita Pizza is to many the true Italian flag. One of the most loved Italian dishes, it just takes a few simple ingredients and you get insanely delicious results! You just can't go wrong with that tomato, basil and fresh mozzarella combo."],
+                     
+                     
+                      ["Title":"Lasagna","Image":#imageLiteral(resourceName: "lasagna_620x350_81508846322.jpg"),"URL":"https://www.youtube.com/watch?v=U0zk2H3mMoA","Des":"In this video, we're cooking Lasagne at home, without using an oven. This vegetable lasagne has an Indian twist, just the way we like it. Plus point of the recipe is, we will be making our very own eggless pasta dough, and then the lasagna sheets. This recipe will be made from scratch."],
+                      
+                       ["Title":"Ossobuco alla Milanese","Image":#imageLiteral(resourceName: "ossobuco-alla-milanese.jpg"),"URL":"https://www.youtube.com/watch?v=9GUTC2Qwrf0","Des":"L’ossobuco alla milanese con risotto giallo è una ricetta tipica della città di Milano e, più in generale, della Lombardia: Nella cucina di GialloZafferano abbiamo avuto l'onore di ospitare un grande chef, Alessandro Negrini, che ci ha svelato tutti i suoi segreti per preparare questa pietanza, sostanziosa e dal gusto robusto."],
+                       
+                        ["Title":"Gelato","Image": #imageLiteral(resourceName: "italian-gelato.jpg"),"URL":"https://www.youtube.com/watch?v=LVtRrvmSJ8M","Des":"To get this complete recipe with instructions and measurements, check out my website: http://www.LauraintheKitchen.com"],
+                        
+                        ["Title":"Panzanella","Image":#imageLiteral(resourceName: "panzanella-salad.jpg"),"URL":"https://www.youtube.com/watch?v=J-Rn9DN3IjU","Des":"Learn how to make a Crispy Panzanella Salad recipe! Go to http://foodwishes.blogspot.com/2014/0... for the ingredient amounts, extra information, and many, many more video recipes! I hope you enjoy this easy Tuscan Bread & Tomato Salad video!"],
+                         
+                          ["Title":"Focaccia","Image": #imageLiteral(resourceName: "focaccia-liguria.jpg"),"URL":"https://www.youtube.com/watch?v=-DmVedGWAnU","Des":"Italian focaccia bread is a fantastic gateway to the the world of bread making. This recipe simplifies the dough making process, step-by-step, in a way that's easy to understand. The results are truly amazing."],
+                          
+
+                           ["Title":"Spaghetti alla Carbonara","Image": #imageLiteral(resourceName: "spaghetti-carbonara.jpg"),"URL":"https://www.youtube.com/watch?v=D_2DBLAt57c","Des":"Jamie shows us how to make a truly authentic, traditional and classic Spaghetti Carbonara. The dish uses two traditional Italian ingredients; a cured meat called Guanciale and Roma’s staple cheese, Pecorino Romano. This is the perfect Italian treat! Enjoy!"],
+                          
+                           ["Title":"Cicchetti","Image":#imageLiteral(resourceName: "italian-cicchetti.jpg") ,"URL":"https://www.youtube.com/watch?v=2p5OwZK0W2M","Des":" A tipical venetian recipe, suitable for cocktail time or dinner. "],
+                           
+                           ["Title":"Burrata","Image": #imageLiteral(resourceName: "burrata-cheese.jpg"),"URL":"https://www.youtube.com/watch?v=uNxeeS2DMY8","Des":" Love the creamy taste of fresh burrata cheese but hate the costly price tag that comes with it? Then follow along as Brandon Boudet, executive chef and co-owner of Dominick's Restaurant, demonstrates how to make the Italian cheese yourself. It's simpler than you think! Watch the video, and then get the burrata recipe."],
+                           
+                           ["Title":"Arancini","Image":#imageLiteral(resourceName: "1426576976-4881966.jpeg") ,"URL":"https://www.youtube.com/watch?v=4lZ7y4FN3eM","Des":"Another beautiful Italian recipe direct from the grand master of Food Tube - Gennaro. These simple Arancini are a great way to use up left over Risotto Bianco and make a fantastic meal in themselves. Try them at your family parties this Thanksgiving weekend."],
+                           
+                               ["Title":"Saltimbocca","Image": #imageLiteral(resourceName: "1426578956-9821121.jpeg"),"URL":"https://www.youtube.com/watch?v=rm9q_JLXiAY","Des":"Today I would like to share with you my Chicken Saltimbocca Recipe."],
+                           
+                                ["Title":"Ossobuco","Image":#imageLiteral(resourceName: "ossobuco-b-768x700.jpg") ,"URL":"https://www.youtube.com/watch?v=b3_NQiH_WMc","Des":"Ossobuco is a top dish from Italian cuisine. You do not have to travel to Italy to enjoy a delicious 'ossobuco alla Milanese' again. With these tips and recipe, you simply get the southern flavors to your kitchen. And that with our own Belgian veal."],
+                                
+                                
+                                 ["Title":"Cotoletta alla Milanese","Image": #imageLiteral(resourceName: "cotoletta.jpg"),"URL":"https://www.youtube.com/watch?v=CqqoZDfUoPM","Des":"The Wiener schnitzel"]
+        
+                                 ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Chef/Controllers/Maxican.swift
+++ Chef/Controllers/Maxican.swift
@@ -16,14 +16,36 @@ class Maxican: UICollectionViewController {
           }
     
     var maxican = [
-    ["Title":"Jalebi ","Image":#imageLiteral(resourceName: "Jalebi-Recipe-Rasoi-Menu"),"URL":"","Des":""],
+         ["Title":"Mexican Hot Chocolate","Image":  #imageLiteral(resourceName: "20190905-delish-mexican-hot-chocolate-ehg-1305-jpg-1569007275.jpg"),"URL":"https://www.youtube.com/watch?v=fpgEWNPHF6M","Des":"If you love a bit of heat, this is the only hot cocoa recipe you'll ever . need."],
+        
+    ["Title":"Keto Taco Cups ","Image":#imageLiteral(resourceName: "M-1"),"URL":"https://www.youtube.com/watch?v=4593t8rJhJA","Des":"Cheese shells are all the rage."],
                  
-                  ["Title":"Medu wada ","Image":#imageLiteral(resourceName: "South_Indian_Medu_Vada_Sambar_with_Medu_Vada_Maker-8_400"),"URL":"","Des":""],
+                  ["Title":"Cheesy Mexican Cauliflower Rice","Image":#imageLiteral(resourceName: "m-2"),"URL":"https://www.youtube.com/watch?v=x1rvbXpAQeQ","Des":"This is a perfect low-carb option that doesn't sacrifice any of the flavor."],
                   
-                   ["Title":"Raas malai ","Image":#imageLiteral(resourceName: "Kesar-Rasmalai"),"URL":"","Des":""],
+                   ["Title":"Turkey Taco Lettuce Wraps","Image":#imageLiteral(resourceName: "m-3"),"URL":"https://www.youtube.com/watch?v=BXS8JM05MWk","Des":"Who needs tortillas?"],
+                   ["Title":"Best-Ever Migas","Image":#imageLiteral(resourceName: "m-4"),"URL":"https://www.youtube.com/watch?v=u8cv5WDxfos","Des":"Breakfast doesn't get much better than this."],
+                   ["Title":"Churro Chips","Image":#imageLiteral(resourceName: "m-5"),"URL":"https://www.youtube.com/watch?v=LNgkwxuxtu8","Des":"HIGHLY recommend dunking in cheesecake dip."],
+                   ["Title":"Keto Taco Casserole","Image":#imageLiteral(resourceName: "m-6"),"URL":"https://www.youtube.com/watch?v=5APAZrA-R3w","Des":"This baby is a total crowd-pleaser."],
+                   ["Title":"Spicy Chicken Taquitos","Image":#imageLiteral(resourceName: "m-7"),"URL":"https://www.youtube.com/watch?v=INNX2tpH1No","Des":"The avocado cream is a non-negotiable."],
                    
-                    ["Title":"Uthapam ","Image":#imageLiteral(resourceName: "big_onion_tomato_uttapam,_tomato_onion_uttapa-11571"),"URL":"","Des":""] ]
-    
+                   ["Title":"Tacos Al Pastor","Image":#imageLiteral(resourceName: "m-8"),"URL":"https://www.youtube.com/watch?v=JOvLXKU6mB4","Des":"Pineapple and pork is always a winning combo."],
+                   ["Title":"Shrimp Enchiladas","Image":#imageLiteral(resourceName: "m-9.png"),"URL":"https://www.youtube.com/watch?v=zpTvp-kIzqs","Des":"Give chicken a break."],
+                   ["Title":"Beef Enchiladas","Image":#imageLiteral(resourceName: "beef-enchiladas-vertical-1535657909"),"URL":"https://www.youtube.com/watch?v=CjoVxMSdfKg","Des":"Pairs perfectly with Jarritos Margaritas."],
+                   ["Title":"Tex-Mex-Inspired Pizza","Image":#imageLiteral(resourceName: "mexican-pizza-228-1547837679.jpg"),"URL":"https://www.youtube.com/watch?v=fa0YvpVeZGU","Des":"Ummm, this is basically our two favorite foods (tacos and pizza) combined!"],
+                  
+                   ["Title":"Chili Cheese Crunchwrap","Image": #imageLiteral(resourceName: "delish-chili-cheese-crunchwrap-pin-1548972033.jpg"),"URL":"https://www.youtube.com/watch?v=A9rjXhuFdkA","Des":"Better than the Taco Bell original..."],
+                  
+                   ["Title":"Pulled Pork Tacos with Pineapple Slaw","Image": #imageLiteral(resourceName: "pulled-pork-1529441523.jpg"),"URL":"https://www.youtube.com/watch?v=m8-FghwjLt0","Des":"You'll want to use the pineapple slaw on everything."],
+                   ["Title":"Posole","Image":#imageLiteral(resourceName: "gallery-1506455950-delish-posole-2.jpg"),"URL":"https://www.youtube.com/watch?v=FGwCQh2h-O8","Des":"Enjoy the comfort of this Mexican favorite without any of the work."],
+                   ["Title":"Perfect Fish Tacos","Image":#imageLiteral(resourceName: "190307-fish-tacos-107-1553283299.jpg"),"URL":"https://www.youtube.com/watch?v=qDFqRxeEn9o","Des":"Fresh avo and corn slaw make these sooo tasty."],
+                   ["Title":"Chicken Enchilada Casserole","Image":#imageLiteral(resourceName: "enchilada-casserole-vertical-1533917088.jpg"),"URL":"https://www.youtube.com/watch?v=g9k_RCIG4iY","Des":"Fresh avo and corn slaw make these sooo tasty."],
+                   ["Title":"Burrito Egg Rolls","Image":#imageLiteral(resourceName: "delish-burrito-egg-rolls-pin-1549490514.jpg"),"URL":"https://www.youtube.com/watch?v=_3zVcyfIheo","Des":"This is the ultimate hybrid!!! American indulgence, meet Mexican fillings."]
+                   
+                   
+                   
+                   ]
+  
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
